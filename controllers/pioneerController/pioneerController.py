4208d7d63620e21f342fe3e5b567ff0162753847
PrasadNR <prasadnr606@yahoo.in>
2019-10-10 18:25:11 +0530

    Line follower code fixed

--- controllers/pioneerController/pioneerController.py
+++ controllers/pioneerController/pioneerController.py
@@ -44,10 +44,15 @@ while (robot.step(timestep) != -1):
 	cameraData = camera.getImage()
 	image = np.frombuffer(cameraData, np.uint8).reshape((camera.getHeight(), camera.getWidth(), 4)) #BGRA
 	
-	redMask = cv2.inRange(image[int(camera.getHeight()/2):camera.getHeight(), :, 2], 16, 200)
-	greenMask = cv2.inRange(image[int(camera.getHeight()/2):camera.getHeight(), :, 1], 0, 100)
-	blueMask = cv2.inRange(image[int(camera.getHeight()/2):camera.getHeight(), :, 0], 0, 100)
-	momentMask = (redMask / 255) * ((greenMask + blueMask) / (2 * 255))
+	red = image[int(camera.getHeight()/2):camera.getHeight(), :, 2]
+	green = image[int(camera.getHeight()/2):camera.getHeight(), :, 1]
+	blue = image[int(camera.getHeight()/2):camera.getHeight(), :, 0]
+
+	hsvImage = cv2.cvtColor(np.dstack((blue, green, red)), cv2.COLOR_BGR2HSV)
+	lowerHSV = np.array([120, 0, 50])
+	upperHSV = np.array([180, 255, 200])
+
+	momentMask = cv2.inRange(hsvImage, lowerHSV, upperHSV)
 	moment = cv2.moments(momentMask, False)
 	
 	try:
