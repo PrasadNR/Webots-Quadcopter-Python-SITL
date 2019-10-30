737eaa870e448c24365b4b6dbb6fa52b0494f576
PrasadNR <prasadnr606@yahoo.in>
2019-10-30 23:37:54 +0530

    Problem with Webots example source code (just translated C code to Python as is)

--- controllers/mavic2proController/mavic2proController.py
+++ controllers/mavic2proController/mavic2proController.py
@@ -1,3 +1,5 @@
+#This is the Python version (which I wrote) of https://github.com/cyberbotics/webots/blob/revision/projects/robots/dji/mavic/controllers/mavic2pro/mavic2pro.c
+
 from controller import *
 import mavic2proHelper
 import cv2
@@ -31,6 +33,9 @@ gyro.enable(TIME_STEP)
 camera_roll_motor = robot.getMotor("camera roll")
 camera_pitch_motor = robot.getMotor("camera pitch")
 
+keyboard = Keyboard();
+keyboard.enable(TIME_STEP);
+
 k_vertical_thrust = 68.5;
 k_vertical_offset = 0.6;
 k_vertical_p = 3.0;
@@ -61,4 +66,35 @@ while (robot.step(timestep) != -1):
 	if not(numpy.isnan(camera_pitch_motor_position)):
 		camera_pitch_motor.setPosition(camera_pitch_motor_position)
 
+	key = keyboard.getKey()
 	
+	if key == keyboard.UP:
+		pitch_disturbance = 2.0
+	if key == keyboard.DOWN:
+		pitch_disturbance = -2.0
+	if key == keyboard.LEFT:
+		yaw_disturbance = -1.3
+	if key == keyboard.RIGHT:
+		yaw_disturbance = 1.3
+	if key == ord('4'):
+		roll_disturbance = 1.0
+	if key == ord('6'):
+		roll_disturbance = -1.0
+	if key == ord('8'):
+		target_altitude += 0.05
+	if key == ord('2'):
+		target_altitude -= 0.05
+
+	roll_input = k_roll_p * numpy.clip(roll, -1.0, 1.0) + roll_acceleration + roll_disturbance
+	pitch_input = k_pitch_p * numpy.clip(pitch, -1.0, 1.0) - pitch_acceleration + pitch_disturbance
+	yaw_input = yaw_disturbance
+	clamped_difference_altitude = numpy.clip(target_altitude - altitude + k_vertical_offset, -1.0, 1.0)
+	vertical_input = k_vertical_p * pow(clamped_difference_altitude, 3.0)
+
+	front_left_motor_input = k_vertical_thrust + vertical_input - roll_input - pitch_input + yaw_input
+	front_right_motor_input = k_vertical_thrust + vertical_input + roll_input - pitch_input - yaw_input
+	rear_left_motor_input = k_vertical_thrust + vertical_input - roll_input + pitch_input - yaw_input
+	rear_right_motor_input = k_vertical_thrust + vertical_input + roll_input + pitch_input + yaw_input
+
+	if not(numpy.isnan(front_left_motor_input)):
+		mavic2proHelper.motorsSpeed(robot, front_left_motor_input, -front_right_motor_input, -rear_left_motor_input, rear_right_motor_input)
