ba40c020d354d3f5c6710c3a67f801db881b8ca3
PrasadNR <prasadnr606@yahoo.in>
2019-11-23 19:40:27 +0530

    Rover needs to follow path without camera

--- pathCSV/rover_waypoint_generator.py
+++ pathCSV/rover_waypoint_generator.py
@@ -1,15 +1,16 @@
 import cv2
+import csv
 
 global clickCoordinates
 clickCoordinates = list()
 
-def click_and_crop(event, x, y, flags, param):
+def click_point(event, x, y, flags, param):
 	if event == cv2.EVENT_LBUTTONDOWN:
 		clickCoordinates.append((x, y))
 
 image = cv2.imread("rover_circuit.jpg")
 cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
+cv2.setMouseCallback("image", click_point)
 
 while True:
 	cv2.imshow("image", image)
@@ -17,4 +18,8 @@ while True:
 	if key == ord(" "):
 		break
 
-print(clickCoordinates)
+with open('waypoints.csv', 'w', newline='') as f:
+	writer = csv.writer(f)
+	writer.writerow(["waypointID", "coordinateX", "coordinateY"])
+	for coordinateID, eachClickCoordinatePair in enumerate(clickCoordinates):
+		writer.writerow([coordinateID, eachClickCoordinatePair[0], eachClickCoordinatePair[1]])
